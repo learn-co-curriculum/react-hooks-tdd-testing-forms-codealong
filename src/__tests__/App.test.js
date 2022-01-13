89e207e35491abf6b7f005cc2f46dbd1412e3908
Ian Hollander <ianholla@gmail.com>
2022-01-13 15:11:49 -0500

    Formatting

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -6,19 +6,19 @@ import App from "../App";
 test("checkbox is initially unchecked", () => {
   render(<App />);
 
-  const addPepperoni = screen.getByRole("checkbox", {name: /add pepperoni/i});
-  
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
   expect(addPepperoni).not.toBeChecked();
-})
+});
 
 test("checkbox appears as checked when user clicks it", () => {
   render(<App />);
 
-  const addPepperoni = screen.getByRole("checkbox", {name: /add pepperoni/i});
-  
+  const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i });
+
   userEvent.click(addPepperoni);
   expect(addPepperoni).toBeChecked();
-})
+});
 
 test("checkbox appears as unchecked when user clicks a second time", () => {
   render(<App />);
@@ -32,7 +32,6 @@ test("checkbox appears as unchecked when user clicks a second time", () => {
   userEvent.click(addPepperoni);
 
   expect(addPepperoni).not.toBeChecked();
-
 });
 
 test("size select element initially displays 'Small'", () => {
@@ -41,7 +40,7 @@ test("size select element initially displays 'Small'", () => {
   const selectSize = screen.getByLabelText(/select size/i);
 
   expect(selectSize).toHaveDisplayValue("Small");
-})
+});
 
 test("select Size dropdown displays the user's selected value", () => {
   render(<App />);
@@ -55,14 +54,14 @@ test("select Size dropdown displays the user's selected value", () => {
   userEvent.selectOptions(selectSize, "large");
 
   expect(selectSize).toHaveDisplayValue("Large");
-})
+});
 
 // "Your Selection" text
 test("'Your Selection' message initially displays 'small cheese'", () => {
   render(<App />);
 
   expect(screen.getByText("small cheese")).toBeInTheDocument();
-})
+});
 
 test("selecting options updates the 'Your selection' message", () => {
   render(<App />);
@@ -77,33 +76,33 @@ test("selecting options updates the 'Your selection' message", () => {
   userEvent.selectOptions(selectSize, "large");
 
   expect(screen.getByText("large pepperoni")).toBeInTheDocument();
-
-})
+});
 
 // "Contact Info" text box
 test("'Contact Info' text box initially displays a placeholder value of 'email address'", () => {
   render(<App />);
 
   expect(screen.getByPlaceholderText(/email address/i)).toBeInTheDocument();
-
-})
+});
 
 test("the page shows information the user types into the contact form field", () => {
   render(<App />);
 
   const contact = screen.getByLabelText(/enter your email address/i);
-  
+
   userEvent.type(contact, "pizzafan@email.com");
 
   expect(contact).toHaveValue("pizzafan@email.com");
-})
+});
 
 // Submit Order button
 test("form contains a 'Submit Order' button", () => {
   render(<App />);
 
-  expect(screen.getByRole("button", { name: /submit order/i })).toBeInTheDocument();
-})
+  expect(
+    screen.getByRole("button", { name: /submit order/i })
+  ).toBeInTheDocument();
+});
 
 test("clicking the Place Order button displays a thank you message", () => {
   render(<App />);
@@ -111,5 +110,4 @@ test("clicking the Place Order button displays a thank you message", () => {
   userEvent.click(screen.getByRole("button", { name: /submit order/i }));
 
   expect(screen.getByText("Thanks for your order!")).toBeInTheDocument();
-})
-
+});
